8dc468d47c692f20db8214ca6999b6885915cbd1
YASIYA-MD-DB-MANAGMENT <darkyasiya@outlook.com>
2026-04-27 19:36:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?KlnFIR',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Fe5saC',
     PORT: process.env.PORT || 7860,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys",
 };
